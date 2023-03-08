838a175c63c03d09a8cde93d5c844f6f70a94efb
Nukun Ukritnukun <n.ukritnukun@gmail.com>
2023-03-08 20:18:29 +0700

    FR2 Code. addAccountToCustomer + Select Account Type + opening Balance Method

--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -1,17 +1,68 @@
 package newbank.server;
+import java.io.*;
 
 import java.util.HashMap;
+
+import static java.lang.System.out;
 
 public class NewBank {
 	
 	private static final NewBank bank = new NewBank();
 	private HashMap<String,Customer> customers;
 	
-	private NewBank() {
+	NewBank() {
 		customers = new HashMap<>();
 		addTestData();
 	}
-	
+
+	private void addAccountToCustomer(String customerId) throws IOException {
+		// Check if customer exists
+		if (customers.containsKey(customerId)) {
+			// Get the customer object
+			Customer customer = customers.get(customerId);
+			// Ask for the account type that wish to open
+			String accountType = selectAccountType();
+			// Ask for initial input
+			double balance = openingBalance();
+			Account account = new Account(accountType, balance);
+
+			// Add the account to the customer's account hashmap
+			customer.addAccount(account);
+		} else {
+			out.println("Customer not found!");
+		}
+	}
+	public String selectAccountType() {
+		out.println("Select the account type by number");
+		out.println("1. Savings account");
+		out.println("2. Checking account");
+		out.println("3. Main account");
+		out.println("4. Return");
+
+		String typeOfAccount;
+		try {
+			BufferedReader in = null;
+			assert false;
+			typeOfAccount = in.readLine();
+			switch (typeOfAccount) {
+				case "1": return "Savings";
+				case "2": return "Checking";
+				case "3": return "Main";
+				case "4": return "Return";
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return "";
+	}
+
+	private double openingBalance() throws IOException {
+		out.println("Input the opening balance that you wish to put");
+		DataInput in = null;
+		assert false;
+		return in.readDouble();
+	}
+
 	private void addTestData() {
 		Customer bhagy = new Customer();
 		bhagy.addAccount(new Account("Main", 1000.0));
