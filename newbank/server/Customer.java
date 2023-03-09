c90f65cb5ad534f68e3e75b4d5b46068f6a7324d
karimdaw1991 <karimdaw91@gmail.com>
2023-03-09 20:37:09 +0100

    Refactor: Add doctrings

--- newbank/server/Customer.java
+++ newbank/server/Customer.java
@@ -4,32 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Customer {
-	
+
 	private ArrayList<Account> accounts;
-	
+
 	public Customer() {
 		accounts = new ArrayList<>();
 	}
-	
+
+	/**
+	 * @return a string of an account
+	 */
 	public String accountsToString() {
 		String s = "";
-		for(Account a : accounts) {
+		for (Account a : accounts) {
 			s += a.toString();
 		}
 		return s;
 	}
-	//display the accounts content into a list
+
+	/**
+	 * display the accounts content into a list
+	 * 
+	 * @return a list containing Accounts
+	 */
 	public List<String> accountsToList() {
-		ArrayList<String> l = new ArrayList<String>() {
-			
-		};
-		for(Account a : accounts) {
+		ArrayList<String> l = new ArrayList<String>();
+		for (Account a : accounts) {
 			l.add(a.toString());
 		}
 		return l;
 	}
 
+	/**
+	 * adds account to list of accounts
+	 * 
+	 * @param account
+	 */
 	public void addAccount(Account account) {
-		accounts.add(account);		
+		accounts.add(account);
 	}
 }
