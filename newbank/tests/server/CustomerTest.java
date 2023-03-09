89e760270bf079bad3187f49ca94a7bbafd1588f
karimdaw1991 <karimdaw91@gmail.com>
2023-03-09 22:16:03 +0100

    Test: adding basick tests for neewbank

---
+++ newbank/tests/server/CustomerTest.java
@@ -0,0 +1,20 @@
+package newbank.tests.server;
+
+import org.junit.Test;
+
+public class CustomerTest {
+    @Test
+    public void testAccountsToList() {
+
+    }
+
+    @Test
+    public void testAccountsToString() {
+
+    }
+
+    @Test
+    public void testAddAccount() {
+
+    }
+}
