aa0d326944388f73b2a3c789be90fd9e74a80bfe
Maria <mcbb22@bath.ac.uk>
2023-03-10 11:33:57 +0000

    New User branch and file created

---
+++ newbank/server/NewUser.java
@@ -0,0 +1,7 @@
+// Use case requirements
+
+// New user can request to register.
+// The system will request user to provide a username and password.
+// The system will check for any conflicts with existing usernames.
+// If conflict, the system will prompt them to choose another username.
+// If no conflict the system adds them to the data store and notifies them.
