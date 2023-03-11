15ad854759883e6507e12d85d52b4f5ef702600f
Nukun Ukritnukun <n.ukritnukun@gmail.com>
2023-03-11 17:57:31 +0700

    modified move function

--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -36,13 +36,50 @@ public class NewBank {
 		}
 		return null;
 	}
+	private void moveMoney(CustomerID customer, String sourceAccount, String destinationAccount, double amount){
+        Customer c = customers.get(customer.getKey());
+		double movingAmount;
+		//check if the amount transfer is less than zero 
+        try {
+            movingAmount = Double.parseDouble(amount);
+            if (movingAmount < 0.0) {
+                return "Re-enter moving amount again.";
+            }
+        } catch (NumberFormatException e) {
+            return "Please re-enter with numbers only.";
+        }
+		//check if the account is exist 
+        try {
+            if (c.checkAccount(sourceAccount) == false || c.checkAccount(destinationAccount) == false) {
+                return "Account doee not exist";
+            }
+
+        } catch (Exception e) {
+            return "Account does not exist";
+        }
+		//check the account value 
+        Double currentBalance = c.getAccountValue();
+		//deduct amount from the moving account 
+        if (currentBalance >= movingAmount) {
+			c.changeBalance(-movingAmount)
+            return "SUCCESS";
+        } else {
+            return "FAIL";
+        }
+    }
 
 	// commands from the NewBank customer are processed in this method
 	public synchronized String processRequest(CustomerID customer, String request) {
 		if(customers.containsKey(customer.getKey())) {
 			switch(request) {
+
+			String[] requestInputs = request.split("\\s+");
+			String command = requestInputs[0];
+
 			case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
+			case "MOVE": return moveMoney(customer, "main", "savings", 2000);
 			default : return "FAIL";
+			
 			}
 		}
 		return "FAIL";
