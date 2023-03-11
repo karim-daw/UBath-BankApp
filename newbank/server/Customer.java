10ca86f8fbd698f59c4983e7711d52ccd4158bf7
Nukun Ukritnukun <n.ukritnukun@gmail.com>
2023-03-11 15:31:51 +0700

    add checkaccount method at Customer.java

--- newbank/server/Customer.java
+++ newbank/server/Customer.java
@@ -3,22 +3,31 @@ package newbank.server;
 import java.util.ArrayList;
 
 public class Customer {
-	
+
 	private ArrayList<Account> accounts;
-	
+
 	public Customer() {
 		accounts = new ArrayList<>();
 	}
-	
+
 	public String accountsToString() {
 		String s = "";
-		for(Account a : accounts) {
+		for (Account a : accounts) {
 			s += a.toString();
 		}
 		return s;
 	}
 
 	public void addAccount(Account account) {
-		accounts.add(account);		
+		accounts.add(account);
+	}
+
+	public boolean checkAccount(String accountName) {
+		for (Account account : accounts) {
+			if (account.getAccountName().equals(accountName)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
