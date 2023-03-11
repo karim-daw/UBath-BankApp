0b4e1c76a3723ca2b927668e58948ae3ee3000dd
karimdaw1991 <karimdaw91@gmail.com>
2023-03-11 19:57:16 +0100

    Add: Requests interface, optional

---
+++ newbank/server/Requests/IRequests.java
@@ -0,0 +1,11 @@
+package newbank.server.Requests;
+
+import java.io.IOException;
+
+public interface IRequests {
+
+    public String handleRequest() throws IOException;
+
+    public void displayRequest();
+
+}
