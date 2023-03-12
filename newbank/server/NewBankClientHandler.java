783da9f254d841bd62a6ab12fdcf3d5a446c27f3
karimdaw1991 <karimdaw91@gmail.com>
2023-03-12 22:14:51 +0100

    Add: attempting to add register function

--- newbank/server/NewBankClientHandler.java
+++ newbank/server/NewBankClientHandler.java
@@ -23,24 +23,26 @@ public class NewBankClientHandler extends Thread {
 		try {
 			while (true) {
 				/*
-				Initial Welcome Screen:
-				* Existing users must login before accessing system
-				* New Users must register a unique username and password before allowed to login
-				*/
+				 * Initial Welcome Screen:
+				 * Existing users must login before accessing system
+				 * New Users must register a unique username and password before allowed to
+				 * login
+				 */
 				String request = userWelcome();
 				// The User is not logged into the system yet so CustomerID is null
 				CustomerID customer = null;
 				// Pricesses the user's response: LOGIN or REGISTER
-				while(true) {
-					if(request.equals("LOGIN")) {
+				while (true) {
+					if (request.equals("LOGIN")) {
 						customer = userLogIn();
-					} 
-					else {	
-					//TO DO: add new user registration method or code
-					// userRegistration()
+					} else if (request.equals("REGISTER")) {
+						customer = userRegistration();
+					} else {
+
 					}
 
-					// if the user has succesfully logged-in, get requests from the user and process them
+					// if the user has succesfully logged-in, get requests from the user and process
+					// them
 					if (customer != null) {
 						while (true) {
 							// Asking for a request and process the request
@@ -60,11 +62,9 @@ public class NewBankClientHandler extends Thread {
 					}
 				}
 			}
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
-		} 
-		finally {
+		} finally {
 			try {
 				in.close();
 				out.close();
@@ -75,7 +75,6 @@ public class NewBankClientHandler extends Thread {
 		}
 	}
 
-	
 	public String userWelcome() throws IOException {
 		out.println("##############################\n");
 		out.println("**** Welcome to New Bank ****\n");
@@ -85,42 +84,71 @@ public class NewBankClientHandler extends Thread {
 		String request = in.readLine();
 		boolean requestValid = false;
 		do {
-			if(request.equals("LOGIN") || request.equals("REGISTER")) {
+			if (request.equals("LOGIN") || request.equals("REGISTER")) {
 				requestValid = true;
-			}
-			else {
+			} else {
 				out.println("Invalid request: Please enter LOGIN or REGISTER");
 				request = in.readLine();
 			}
-		}while (!requestValid);
+		} while (!requestValid);
 		return request;
 	}
-	
+
 	// Login for existing customers
 	public CustomerID userLogIn() throws IOException {
 		// Not a customer yet
+		out.println("THIS IS A TEST___________");
+
 		CustomerID customer = null;
-		//Ask for existing username
-		out.println("Enter Username");
+		// Ask for existing username
+		out.println("Enter Usernamaae");
 		String userName = in.readLine();
 		// ask for existing password
 		out.println("Enter Password");
 		String password = in.readLine();
 		out.println("Please wait while we check your details");
 		customer = bank.checkLogInDetails(userName, password);
-		//Validate login details
-		if(customer == null) {
+		// Validate login details
+		if (customer == null) {
 			out.println("Log In Failed. Invalid Credentials, please try again.");
-		}
-		else {
+		} else {
 			out.println("Log In Successful. What do you want to do?");
 		}
 		return customer;
 	}
-	
-	//TO DO
+
+	// TO DO
 	// Registration for new customers
-	public void userRegistration() throws IOException {
-		
+	public CustomerID userRegistration() throws IOException {
+
+		// flag for registrationed success
+		CustomerID customerID = null;
+
+		// Ask for existing username
+		out.println("Enter Usernasasame");
+		String userName = in.readLine();
+
+		// ask password
+		out.println("Choose Password");
+		String passwordAttempt1 = in.readLine();
+		out.println("Re-Enter Password");
+		String passwordAttempt2 = in.readLine();
+
+		if (!passwordAttempt2.equals(passwordAttempt1)) {
+			out.println("Passwords do not match");
+			return null;
+		}
+		// check if userName already exists, if yesm is registers gets changed to true
+		customerID = bank.registerCustomer(userName, passwordAttempt2);
+		if (customerID != null) {
+			String str = String.format("Registration succesfull. New Customer %s", userName);
+			out.println(str);
+		} else {
+			String str = String.format("Customer name %s already exists, try registerings with a different name",
+					userName);
+			out.println(str);
+		}
+		return customerID;
+
 	}
 }
