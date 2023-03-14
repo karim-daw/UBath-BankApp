e52f84f8733cfa6e77058b73c79d2236244e4f9e
karimdaw1991 <karimdaw91@gmail.com>
2023-03-14 01:09:21 +0100

    Refactor: Display class

---
+++ newbank/server/Display.java
@@ -0,0 +1,39 @@
+package server;
+
+import java.io.PrintWriter;
+
+public class Display {
+
+    // private members
+    private PrintWriter out;
+
+    // contructor
+    public Display(PrintWriter out) {
+        this.out = out;
+    }
+
+    /**
+     * Prints welcome message
+     */
+    public void displayWelcome() {
+        out.println("##############################\n");
+        out.println("**** Welcome to New Bank ****\n");
+        out.println("##############################\n\n");
+        out.println("If you're an existing customer type: LOGIN\n");
+        out.println("If you're a new customer type: REGISTER\n");
+    }
+
+    /**
+     * Displays main menu to user after login
+     */
+    public void displayMainMenu() {
+        out.println("\n");
+        out.println("Select Option...");
+        out.println("SHOWMYACCOUNTS");
+        out.println("NEWACCOUNT");
+        out.println("MOVE");
+        out.println("PAY");
+        out.println("\n");
+    }
+
+}
