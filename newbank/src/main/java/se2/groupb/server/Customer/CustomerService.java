b9904ccd9a61a5f4f764d5ffe8ba391f2b1c2d44
karimdaw1991 <karimdaw91@gmail.com>
2023-03-27 00:34:09 +0200

    Enhancement: Added interface

--- newbank/src/main/java/se2/groupb/server/Customer/CustomerService.java
+++ newbank/src/main/java/se2/groupb/server/Customer/CustomerService.java
@@ -1,5 +1,17 @@
 package se2.groupb.server.customer;
 
-public class CustomerService {
+public interface CustomerService {
+
+    /**
+     * method that changes the password
+     * old password need to be enter
+     * then a new password, twice
+     * 
+     * @param customer
+     * @param requestInputs
+     * @return
+     */
+
+    String changePassword(CustomerDTO customerDTO, String[] requestInputs);
 
 }
