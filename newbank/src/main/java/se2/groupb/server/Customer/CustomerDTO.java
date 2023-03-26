ee7b86ef80ecb4dc4a38101b76fe49c8fcbaa99b
karimdaw1991 <karimdaw91@gmail.com>
2023-03-26 23:46:51 +0200

    Add: customer dto

--- newbank/src/main/java/se2/groupb/server/Customer/CustomerDTO.java
+++ newbank/src/main/java/se2/groupb/server/Customer/CustomerDTO.java
@@ -2,4 +2,19 @@ package se2.groupb.server.customer;
 
 public class CustomerDTO {
 
+    private Long customerID;
+
+    public CustomerDTO(Long customerID) {
+        this.customerID = customerID;
+
+    }
+
+    public Long getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(Long id) {
+        this.customerID = id;
+    }
+
 }
