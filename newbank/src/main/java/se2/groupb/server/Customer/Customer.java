aeeea563878f4b4a65665424f23c88554f2ae09b
karimdaw1991 <karimdaw91@gmail.com>
2023-03-27 00:36:20 +0200

    moved change password servce

--- newbank/src/main/java/se2/groupb/server/Customer/Customer.java
+++ newbank/src/main/java/se2/groupb/server/Customer/Customer.java
@@ -143,43 +143,4 @@ public class Customer {
 
 	}
 
-	/**
-	 * method that changes the password
-	 * old password need to be enter
-	 * then a new password, twice
-	 * 
-	 * @param customer
-	 * @param requestInputs
-	 * @return
-	 */
-
-	public String changePassword(CustomerID customer, String[] requestInputs) {
-		// check if the command is correct
-		// return infinite loop of null, why ?
-		int inputLength = requestInputs.length;
-		if (inputLength < 4) {
-			return "FAIL. Please enter your old password and twice your new password after the command.";
-		}
-
-		String oldPassword = requestInputs[1];
-		String newPassword = requestInputs[2];
-		String confirmNewPassword = requestInputs[3];
-		Customer c = customers.get(customer.getKey());
-
-		// check if the old password is correct
-		if (!c.getPassword().equals(oldPassword)) {
-			return "FAIL. The old password is incorrect.";
-		}
-
-		// check if the two new password inputs match.
-		if (!newPassword.equals(confirmNewPassword)) {
-			return "FAIL. Password confirmation does not match.";
-		}
-
-		else {
-			c.setPassword(newPassword);
-			return "SUCCESS new password is: " + c.getPassword();
-		}
-	}
-
 }
