84c9115dfb03d828bd1c076deb117943fae69d88
karimdaw1991 <karimdaw91@gmail.com>
2023-03-27 21:01:20 +0200

    Add: username too customerDTO

--- newbank/src/main/java/se2/groupb/server/Customer/CustomerDTO.java
+++ newbank/src/main/java/se2/groupb/server/Customer/CustomerDTO.java
@@ -3,14 +3,20 @@ package se2.groupb.server.customer;
 public class CustomerDTO {
 
     private Long customerID;
+    private String username;
 
-    public CustomerDTO(Long customerID) {
+    public CustomerDTO(Long customerID, String username) {
         this.customerID = customerID;
+        this.username = username;
 
     }
 
     public Long getCustomerID() {
         return customerID;
+    }
+
+    public String getCustomerName() {
+        return username;
     }
 
 }
