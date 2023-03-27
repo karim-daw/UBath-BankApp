6f7bc0e11fe74301e7c0fa54ed9f393cd88291aa
karimdaw1991 <karimdaw91@gmail.com>
2023-03-27 23:41:59 +0200

    naming: change accountid

--- newbank/src/main/java/se2/groupb/server/Account/AccountDTO.java
+++ newbank/src/main/java/se2/groupb/server/Account/AccountDTO.java
@@ -3,20 +3,21 @@ package se2.groupb.server.account;
 import java.math.BigDecimal;
 
 public class AccountDTO {
-    private Long id;
+    private Long accountID;
     private int accountNumber;
     private BigDecimal balance;
     private String accountType;
 
-    public AccountDTO(Long id, int accountNumber, BigDecimal balance, String accountType) {
-        this.id = id;
+    public AccountDTO(Long accountID, int accountNumber, BigDecimal balance, String accountType) {
+        this.accountID = accountID;
+        this.accountID = accountID;
         this.accountNumber = accountNumber;
         this.balance = balance;
         this.accountType = accountType;
     }
 
-    public Long getId() {
-        return id;
+    public Long getAccountId() {
+        return accountID;
     }
 
     public int getAccountNumber() {
