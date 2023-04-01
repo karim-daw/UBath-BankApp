1a247a018e8e9ffb76b062248c0ba6dd1f8ac066
karimdaw1991 <karimdaw91@gmail.com>
2023-04-01 22:11:19 +0200

    add comms object in controller

--- newbank/src/main/java/se2/groupb/server/customer/CustomerController.java
+++ newbank/src/main/java/se2/groupb/server/customer/CustomerController.java
@@ -1,34 +1,37 @@
 package se2.groupb.server.customer;
 
-import java.util.*;
+import java.util.UUID;
 
-import se2.groupb.server.account.Account;
+import se2.groupb.server.NewBank;
+import se2.groupb.server.UserInput;
 
 public class CustomerController {
 
     // fields
     private final CustomerServiceImpl customerService;
+    private final NewBank bank;
+    public UserInput comms;
 
     // Constructor
     public CustomerController(CustomerServiceImpl customerService) {
         this.customerService = customerService;
+        bank = NewBank.getBank();
     }
-    
+
     public UUID checkLogInDetails(CustomerDTO customerDto) {
-    	return customerService.findCustomer(customerDto);	
-	}
-    
-    
+        return customerService.findCustomer(customerDto);
+    }
+
     public boolean isLoggedIn(UUID customerID) {
-    	return customerService.isLoggedIn(customerID);
+        return customerService.isLoggedIn(customerID);
     }
-    
+
     /*
-    public ArrayList<Account> getAccounts(UUID customerID){
-    	return customerService.getAccounts(customerID);
-    }
-    */
-    
+     * public ArrayList<Account> getAccounts(UUID customerID){
+     * return customerService.getAccounts(customerID);
+     * }
+     */
+
     /**
      * displays the customers accounts as a list
      * 
