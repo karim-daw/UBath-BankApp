b918c3738345631ee89cebd311fa48ce88bd4c89
karimdaw1991 <karimdaw91@gmail.com>
2023-04-02 17:27:31 +0200

    REMOVE: UUID DEPENDANCY

--- newbank/src/main/java/se2/groupb/server/customer/CustomerDTO.java
+++ newbank/src/main/java/se2/groupb/server/customer/CustomerDTO.java
@@ -5,30 +5,26 @@ import java.util.UUID;
 //import java.util.*;
 //import se2.groupb.server.Account.Account;
 
-
 //Subset of Domain for data transfer between layers
 public final class CustomerDTO {
-	private String username;
-	private String password;
-	private UUID customerID;
+    private String username;
+    private String password;
+    private UUID customerID;
 
-	//Constructor
+    // Constructor
     public CustomerDTO(String username, String password) {
-    	this.username = username;
+        this.username = username;
         this.password = password;
-    }
-    
-    public CustomerDTO(UUID customerID) {
-        this.customerID = customerID;
+
     }
 
     public String getUsername() {
-		return this.username;
-	}
-    
+        return this.username;
+    }
+
     public String getPassword() {
-		return this.password;
-	}
+        return this.password;
+    }
 
     public UUID getCustomerID() {
         return customerID;
