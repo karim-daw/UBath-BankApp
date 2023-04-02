60a843db206ad71dc0b472344ba3b92cec3aa47b
karimdaw1991 <karimdaw91@gmail.com>
2023-04-02 23:09:03 +0200

    update login logout

--- newbank/src/main/java/se2/groupb/server/customer/CustomerService.java
+++ newbank/src/main/java/se2/groupb/server/customer/CustomerService.java
@@ -2,30 +2,12 @@ package se2.groupb.server.customer;
 
 import java.util.*;
 
-//import se2.groupb.server.account.Account;
-
-
-    /**
-     * @param customerID
-     * @return
-     */
-    public boolean loginCustomer(UUID customerID);
-
-    /**
-     * @param customerID
-     * @return
-     */
-    public boolean logoutCustomer(UUID customerID);
-
-    // public ArrayList<Account> getAccounts(CustomerDTO customerDto);
-
-
 public interface CustomerService {
-	
-	UUID userLogin(CustomerDTO customerDto);
-	
-	public void userLogout(UUID customerID);
-    
+
+    public UUID userLogin(CustomerDTO customerDto);
+
+    public void userLogout(UUID customerID);
+
     /**
      * returns a string representation in the form of a list of accounts and prints
      * them
@@ -34,7 +16,6 @@ public interface CustomerService {
      * @return
      */
     public String displayAccounts(UUID customerID);
-    
 
     /**
      * method that changes the password
@@ -48,6 +29,6 @@ public interface CustomerService {
 
     public String changePassword(CustomerDTO customerDTO, String[] requestInputs);
 
-
     public String toString();
+
 }
