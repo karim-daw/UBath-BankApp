1b00ae1d1f9d63f7d97b83039493afa1ae98887f
karimdaw1991 <karimdaw91@gmail.com>
2023-04-03 00:19:09 +0200

    clean up

--- newbank/src/main/java/se2/groupb/server/customer/CustomerDTO.java
+++ newbank/src/main/java/se2/groupb/server/customer/CustomerDTO.java
@@ -1,22 +1,16 @@
 package se2.groupb.server.customer;
 
-//import java.util.*;
-//import se2.groupb.server.Account.Account;
-
 //Subset of Domain for data transfer between layers
 public final class CustomerDTO {
 
-	private String username;
-	private String password;
-
+    private String username;
+    private String password;
 
     // Constructor
     public CustomerDTO(String username, String password) {
         this.username = username;
         this.password = password;
     }
-    
-
 
     public String getUsername() {
         return this.username;
