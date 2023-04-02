f3ec6843dfef837a1b5a1c360411a771622bd4b9
karimdaw1991 <karimdaw91@gmail.com>
2023-04-02 20:48:50 +0200

    Add: Skeleton code for transactions

--- newbank/src/main/java/se2/groupb/server/transaction/Transaction.java
+++ newbank/src/main/java/se2/groupb/server/transaction/Transaction.java
@@ -1,8 +1,37 @@
 package se2.groupb.server.transaction;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 public class Transaction {
 
-    private customerID;
-    private sourceAccountID;
-    private targetAccountID; // payee id
+    private final UUID sourceAccountID;
+    private final UUID targetAccountID; // payee id
+    private final UUID transactionID;
+    private final BigDecimal ammount;
+
+    public Transaction(UUID sourceAccountID, UUID targetAccountID, BigDecimal ammount) {
+        this.sourceAccountID = sourceAccountID;
+        this.targetAccountID = targetAccountID;
+        this.ammount = ammount;
+        this.transactionID = UUID.randomUUID();
+
+    }
+
+    public BigDecimal getAmmount() {
+        return ammount;
+    }
+
+    public UUID getTransactionID() {
+        return transactionID;
+    }
+
+    public UUID getSourceAccountID() {
+        return sourceAccountID;
+    }
+
+    public UUID getTargetAccountID() {
+        return targetAccountID;
+    }
+
 }
