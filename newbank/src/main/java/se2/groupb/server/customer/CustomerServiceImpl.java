1294f4d89df3b54291b40baeac99c88a2af93550
karimdaw1991 <karimdaw91@gmail.com>
2023-04-03 00:19:57 +0200

    accepting uuid on service level

--- newbank/src/main/java/se2/groupb/server/customer/CustomerServiceImpl.java
+++ newbank/src/main/java/se2/groupb/server/customer/CustomerServiceImpl.java
@@ -1,11 +1,6 @@
 package se2.groupb.server.customer;
 
 import java.util.*;
-import se2.groupb.server.NewBank;
-import se2.groupb.server.NewBankClientHandler;
-import se2.groupb.server.account.Account;
-import se2.groupb.server.account.AccountDTO;
-import se2.groupb.server.repository.CustomerRepository;
 
 public class CustomerServiceImpl implements CustomerService {
 
@@ -20,42 +15,43 @@ public class CustomerServiceImpl implements CustomerService {
     // Temp HashMap Customer Repo
     private final HashMap<String, Customer> theCustomers;
 
-    //private final HashMap<String, Account> theAccounts;
-    //Temp constructor using HashMap as Customer Repo
-    public CustomerServiceImpl(HashMap<String,Customer> customers) {
+    // private final HashMap<String, Account> theAccounts;
+    // Temp constructor using HashMap as Customer Repo
+    public CustomerServiceImpl(HashMap<String, Customer> customers) {
         this.theCustomers = customers;
     }
-    
+
     public UUID userLogin(CustomerDTO customerDto) {
-		//CustomerService checks HashMap if they have a customer with the entered username & password
-		//If they do then provide the UUID else return null
-    	String username = customerDto.getUsername();
-    	String password = customerDto.getPassword();
-    	UUID customerID = null;
-    	for (HashMap.Entry<String, Customer> item : theCustomers.entrySet()) {
-    		String item_username = item.getValue().getUsername();
-    		String item_password = item.getValue().getPassword();
-    		if ((item_username.equals(username)) && (item_password.equals(password))) {
-    			item.getValue().setloggedInStatus(true);
-    			customerID =item.getValue().getCustomerID();
-    			break;
-    		}
-		}
-    	return customerID;
+        // CustomerService checks HashMap if they have a customer with the entered
+        // username & password
+        // If they do then provide the UUID else return null
+        String username = customerDto.getUsername();
+        String password = customerDto.getPassword();
+        UUID customerID = null;
+        for (HashMap.Entry<String, Customer> item : theCustomers.entrySet()) {
+            String item_username = item.getValue().getUsername();
+            String item_password = item.getValue().getPassword();
+            if ((item_username.equals(username)) && (item_password.equals(password))) {
+                item.getValue().setloggedInStatus(true);
+                customerID = item.getValue().getCustomerID();
+                break;
+            }
+        }
+        return customerID;
     }
-    
-    //returns the Customer object corresponding to the CustomerID provided
+
+    // returns the Customer object corresponding to the CustomerID provided
     public Customer getCustomer(UUID customerID) {
-    	Customer customer = null;
-    	for (HashMap.Entry<String, Customer> cust : theCustomers.entrySet()) {
-    		UUID custID = cust.getValue().getCustomerID();
-    		if (custID.equals(customerID)){
-    			customer = cust.getValue();
-    		}
-    	}
-    	return customer;
+        Customer customer = null;
+        for (HashMap.Entry<String, Customer> cust : theCustomers.entrySet()) {
+            UUID custID = cust.getValue().getCustomerID();
+            if (custID.equals(customerID)) {
+                customer = cust.getValue();
+            }
+        }
+        return customer;
     }
-    
+
     /**
      * displays accounts as a list
      * 
@@ -64,24 +60,21 @@ public class CustomerServiceImpl implements CustomerService {
      */
 
     @Override
-    public String displayAccounts(UUID customerID){
-        //Customer customer = this.customerRepository.findByCustomerID(customerID); 
-        Customer customer = theCustomers.get(customerID.toString()); //temp repo
+    public String displayAccounts(UUID customerID) {
+        // Customer customer = this.customerRepository.findByCustomerID(customerID);
+        Customer customer = theCustomers.get(customerID.toString()); // temp repo
         if (customer.accountsToList().isEmpty()) {
-        	return "You have no accounts to display." ;
+            return "You have no accounts to display.";
+        } else {
+            return customer.accountsToString();
         }
-        else {
-        	return customer.accountsToString();
-        } 
-    } 
-    
-    
-    public void userLogout(UUID customerID) {
-    	Customer customer = theCustomers.get(customerID.toString());
-    	customer.setloggedInStatus(false);
-    	customerID = null;
     }
-    
+
+    public void userLogout(UUID customerID) {
+        Customer customer = theCustomers.get(customerID.toString());
+        customer.setloggedInStatus(false);
+        customerID = null;
+    }
 
     /**
      * method that changes the password
@@ -94,7 +87,7 @@ public class CustomerServiceImpl implements CustomerService {
      */
 
     @Override
-    public String changePassword(CustomerDTO customerDTO, String[] requestInputs) {
+    public String changePassword(UUID customerID, String[] requestInputs) {
         // check if the command is correct
         // return infinite loop of null, why ?
         int inputLength = requestInputs.length;
@@ -106,10 +99,7 @@ public class CustomerServiceImpl implements CustomerService {
         String newPassword = requestInputs[2];
         String confirmNewPassword = requestInputs[3];
 
-        // Customer customer =
         // customerRepository.findByCustomerID(customerDTO.getCustomerID());
-
-        UUID customerID = customerDTO.getCustomerID();
         Customer customer = theCustomers.get(customerID.toString());
 
         // check if the old password is correct
@@ -127,9 +117,8 @@ public class CustomerServiceImpl implements CustomerService {
             return "SUCCESS new password is: " + customer.getPassword();
         }
     }
-    
-    
-    //printing out the Customer HashMap for checking
+
+    // printing out the Customer HashMap for checking
 
     public String toString() {
         String s = "";
