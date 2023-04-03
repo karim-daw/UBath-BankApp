bae5040d508a2f0a62f01a0826ae1da044e29b85
karimdaw1991 <karimdaw91@gmail.com>
2023-04-03 20:12:11 +0200

    formatting

--- newbank/src/main/java/se2/groupb/server/account/AccountController.java
+++ newbank/src/main/java/se2/groupb/server/account/AccountController.java
@@ -8,7 +8,6 @@ import se2.groupb.server.customer.*;
 import se2.groupb.server.repository.CustomerRepository;
 import se2.groupb.server.repository.AccountRepository;
 */
-import se2.groupb.server.account.*;
 import se2.groupb.server.UserInput;
 
 // Presentation layer: Takes user inputs and displays system response
@@ -23,40 +22,37 @@ public class AccountController {
         this.accountService = accountService; // business logic
         this.comms = comms;
     }
-    
-    // methods
-    
-   
-    /*
-    public void creditAmount(AccountDTO accountDTO, double amount) {
 
-        Long accountID = accountDTO.getAccountId();
-        boolean success = accountService.deposit(accountID, amount);
-        if (success) {
-            // display success message to user
-            System.out.println("Deposit successful");
-        } else {
-            // display error message to user
-            System.out.println("Deposit failed");
-        }
-    }
-	*/
-    
+    // methods
+
     /*
-    public void debitAmount(Long accountId, double amount) {
-        boolean success = accountService.withdraw(accountId, amount);
-        if (success) {
-            // display success message to user
-            System.out.println("Withdrawal successful");
-        } else {
-            // display error message to user
-            System.out.println("Withdrawal failed");
-        }
-    }
-	*/
-	
+     * public void creditAmount(AccountDTO accountDTO, double amount) {
+     * 
+     * Long accountID = accountDTO.getAccountId();
+     * boolean success = accountService.deposit(accountID, amount);
+     * if (success) {
+     * // display success message to user
+     * System.out.println("Deposit successful");
+     * } else {
+     * // display error message to user
+     * System.out.println("Deposit failed");
+     * }
+     * }
+     */
+
+    /*
+     * public void debitAmount(Long accountId, double amount) {
+     * boolean success = accountService.withdraw(accountId, amount);
+     * if (success) {
+     * // display success message to user
+     * System.out.println("Withdrawal successful");
+     * } else {
+     * // display error message to user
+     * System.out.println("Withdrawal failed");
+     * }
+     * }
+     */
+
     // other methods for displaying customer details, transaction history, etc.
-    
-    
-    
+
 }
