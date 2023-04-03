2237a8673822da672f4cc19c22c3e969d69ac875
karimdaw1991 <karimdaw91@gmail.com>
2023-04-03 20:15:59 +0200

    implemented repo

--- newbank/src/main/java/se2/groupb/server/repository/CustomerRepositoryImpl.java
+++ newbank/src/main/java/se2/groupb/server/repository/CustomerRepositoryImpl.java
@@ -9,9 +9,9 @@ import se2.groupb.server.customer.CustomerDTO;
 
 // TODO: see if we can add the dummy data for the customer here
 
-public class CustomerRepositoryImpl implements CustomerRepository {
-	
-	// Temp HashMap Customer Repo
+public class CustomerRepositoryImpl implements EntityRepository<Customer, CustomerDTO> {
+
+    // Temp HashMap Customer Repo
     private final HashMap<String, Customer> theCustomers;
 
     // private final HashMap<String, Account> theAccounts;
@@ -22,61 +22,67 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
     /**
      * Searches the Customer Data Store by CustomerID
+     * 
      * @param customerID
      * @return customer from database
      */
-    public Customer findByCustomerID(UUID customerID) {
-    	return theCustomers.get(customerID.toString());
+    @Override
+    public Customer findByID(UUID customerID) {
+        return theCustomers.get(customerID.toString());
     }
-    
+
     /**
      * Searches the Customer Data Store by Username & Password
+     * 
      * @param customerDto
      * @return customer from database
      */
-    public Customer findByCustomerDTO(CustomerDTO customerDto) {
-    	Customer customer = null;
-    	String target_username = customerDto.getUsername();
-    	String target_password = customerDto.getPassword();
-    	
-	    for (HashMap.Entry<String, Customer> cust : theCustomers.entrySet()) {
-	        String cust_username = cust.getValue().getUsername();
-	        String cust_password = cust.getValue().getPassword();
-	        if ((cust_username.equals(target_username)) && (cust_password.equals(target_password))) {
-	        	cust.getValue().setloggedInStatus(true);
-	            customer = cust.getValue();
-	            break;
-	        }
-	    }
-	    return customer;
+    @Override
+    public Customer findByDTO(CustomerDTO customerDto) {
+        Customer customer = null;
+        String target_username = customerDto.getUsername();
+        String target_password = customerDto.getPassword();
+
+        for (HashMap.Entry<String, Customer> cust : theCustomers.entrySet()) {
+            String cust_username = cust.getValue().getUsername();
+            String cust_password = cust.getValue().getPassword();
+            if ((cust_username.equals(target_username)) && (cust_password.equals(target_password))) {
+                cust.getValue().setloggedInStatus(true);
+                customer = cust.getValue();
+                break;
+            }
+        }
+        return customer;
     }
-    
+
     /**
      * Searches if the Username is already taken
+     * 
      * @param username
      * @return
      */
     public boolean duplicateUsername(String username) {
-    	for (HashMap.Entry<String, Customer> cust : theCustomers.entrySet()) {
-	        String cust_username = cust.getValue().getUsername();
-	        if (cust_username.equals(username)) {
-	        	return true;
-	        }
-	    }
-    	return false;
+        for (HashMap.Entry<String, Customer> cust : theCustomers.entrySet()) {
+            String cust_username = cust.getValue().getUsername();
+            if (cust_username.equals(username)) {
+                return true;
+            }
+        }
+        return false;
     }
-    
+
     /**
      * saves new customer into database
+     * 
      * @param customer
      * @return
      */
-    public boolean saveNewCustomer(Customer newCustomer) {
-    	if (findByCustomerID(newCustomer.getCustomerID())==null) {
-    		theCustomers.put(newCustomer.getCustomerID().toString(), newCustomer);
-    		return true;
-    	}
-    	return false;
+    public boolean save(Customer newCustomer) {
+        if (findByID(newCustomer.getCustomerID()) == null) {
+            theCustomers.put(newCustomer.getCustomerID().toString(), newCustomer);
+            return true;
+        }
+        return false;
     }
 
 }
