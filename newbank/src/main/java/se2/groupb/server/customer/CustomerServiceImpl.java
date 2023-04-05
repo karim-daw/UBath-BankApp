40cbe819769b50a22f30c06deb5e696e653acb5b
karimdaw1991 <karimdaw91@gmail.com>
2023-04-05 14:58:02 +0200

    Fixed infinite bug, error checking wasnt worrking

--- newbank/src/main/java/se2/groupb/server/customer/CustomerServiceImpl.java
+++ newbank/src/main/java/se2/groupb/server/customer/CustomerServiceImpl.java
@@ -1,9 +1,6 @@
 package se2.groupb.server.customer;
 
-import java.util.List;
-import java.util.UUID;
-
-import se2.groupb.server.account.Account;
+import java.util.*;
 import se2.groupb.server.repository.CustomerRepositoryImpl;
 
 public class CustomerServiceImpl implements CustomerService {
@@ -18,156 +15,79 @@ public class CustomerServiceImpl implements CustomerService {
 
     // methods
 
+    // returns the Customer object corresponding to the CustomerID provided
     /**
-     * Returns the Customer object corresponding to the CustomerID provided.
+     * returns Customer object from DataStore with the required ID
      * 
-     * @param customerID The ID of the customer to retrieve.
-     * @return The customer with the specified ID.
-     * @throws IllegalArgumentException if customerID is null or not found in the
-     *                                  repository.
+     * @param customerID
+     * @return Customer
      */
     @Override
     public Customer getCustomerByID(UUID customerID) {
-        if (customerID == null) {
-            throw new IllegalArgumentException("Customer ID cannot be null.");
-        }
-        Customer customer = customerRepository.findByID(customerID);
-        if (customer == null) {
-            throw new IllegalArgumentException("Customer not found for ID " + customerID.toString() + ".");
-        }
-        return customer;
+        return customerRepository.findByID(customerID);
     }
 
     /**
-     * Returns the Customer object corresponding to the provided DTO (username &
-     * password).
+     * returns Customer object from DataStore with the required DTO (username &
+     * password)
      * 
-     * @param customerDto The DTO of the customer to retrieve.
-     * @return The customer with the specified DTO.
-     * @throws IllegalArgumentException if customerDto is null or not found in the
-     *                                  repository.
+     * @param customerDto
+     * @return
      */
     @Override
     public Customer getCustomerbyDTO(CustomerDTO customerDto) {
-        if (customerDto == null) {
-            throw new IllegalArgumentException("Customer DTO cannot be null.");
-        }
-        Customer customer = customerRepository.findByDTO(customerDto);
-        if (customer == null) {
-            throw new IllegalArgumentException("Customer not found for DTO " + customerDto.toString() + ".");
-        }
-        return customer;
+        return customerRepository.findByDTO(customerDto);
     }
 
-    /**
-     * Returns the Customer object corresponding to the provided username.
-     * 
-     * @param customerUsername The username of the customer to retrieve.
-     * @return The customer with the specified username.
-     * @throws IllegalArgumentException if customerUsername is null or not found in
-     *                                  the repository.
-     */
     @Override
     public Customer getCustomerbyName(String customerUsername) {
-        if (customerUsername == null) {
-            throw new IllegalArgumentException("Customer username cannot be null.");
-        }
-        Customer customer = customerRepository.findByName(customerUsername);
-        if (customer == null) {
-            throw new IllegalArgumentException("Customer not found for username " + customerUsername + ".");
-        }
-        return customer;
+        return customerRepository.findByName(customerUsername);
     }
 
     /**
-     * Returns true if a customer with the provided username already exists in the
-     * repository.
+     * Returns true if duplicate username found in Customer Data Store
      * 
-     * @param username The username to check for duplicates.
-     * @return true if the username is a duplicate, false otherwise.
-     * @throws IllegalArgumentException if username is null.
+     * @param username
+     * @return boolean
      */
     @Override
     public boolean duplicateUsername(String username) {
-        if (username == null) {
-            throw new IllegalArgumentException("Username cannot be null.");
-        }
         return customerRepository.duplicateUsername(username);
     }
 
     /**
      * Returns true if a new customer has been added to the Customer Data Store
      * 
-     * @param customerDto
+     * @param customer
      * @return boolean
      */
     public boolean addNewCustomer(CustomerDTO customerDto) {
-        // Check if the provided customer DTO is null
-        if (customerDto == null) {
-            throw new IllegalArgumentException("CustomerDTO cannot be null");
-        }
-
-        // Check if the provided username is null or empty
-        String username = customerDto.getUsername();
-        if (username == null || username.isEmpty()) {
-            throw new IllegalArgumentException("Username cannot be null or empty");
-        }
-
-        // Check if the provided password is null or empty
-        String password = customerDto.getPassword();
-        if (password == null || password.isEmpty()) {
-            throw new IllegalArgumentException("Password cannot be null or empty");
-        }
-
-        // Check if a customer with the same username already exists in the repository
-        if (customerRepository.findByDTO(customerDto) != null) {
-            throw new IllegalArgumentException("A customer with the same username already exists");
-        }
-
-        // Create a new customer object from the provided DTO
         Customer newCustomer = new Customer(customerDto);
-
-        // Save the new customer in the repository and return true if successful
         if (customerRepository.save(newCustomer)) {
             return true;
         }
-
-        // If saving the new customer was not successful, throw an exception with an
-        // informative error message
-        throw new RuntimeException("Failed to add a new customer to the repository");
+        return false;
     }
 
     /**
-     * Displays accounts as a list.
+     * displays accounts as a list
      * 
-     * @param customerID ID of the customer
-     * @return A string representing the accounts
+     * @param customer
+     * @return
      */
     @Override
     public String displayAccounts(UUID customerID) {
         Customer customer = customerRepository.findByID(customerID);
-        if (customer == null) {
-            return "ERROR: Customer does not exist.";
-        }
-        List<Account> accounts = customer.getAccounts();
-        if (accounts.isEmpty()) {
+        if (customer.accountsToList().isEmpty()) {
             return "You have no accounts to display.";
         } else {
             return customer.accountsToString();
         }
     }
 
-    /**
-     * Logs the customer out.
-     * 
-     * @param customerID ID of the customer
-     */
     @Override
     public void userLogout(UUID customerID) {
         Customer customer = customerRepository.findByID(customerID);
-        if (customer == null) {
-            throw new IllegalArgumentException("ERROR: Customer does not exist during logout.");
-        }
         customer.setloggedInStatus(false);
     }
 
@@ -187,8 +107,7 @@ public class CustomerServiceImpl implements CustomerService {
         // return infinite loop of null, why ?
         int inputLength = requestInputs.length;
         if (inputLength < 4) {
-            throw new IllegalArgumentException(
-                    "FAIL. Please enter your old password and twice your new password after the command.");
+            return "FAIL. Please enter your old password and twice your new password after the command.";
         }
 
         String oldPassword = requestInputs[1];
@@ -198,16 +117,20 @@ public class CustomerServiceImpl implements CustomerService {
         // customerRepository.findByCustomerID(customerDTO.getCustomerID());
         Customer customer = getCustomerByID(customerID);
 
-        if (!customer.getPassword().equalsIgnoreCase(oldPassword)) {
-            throw new IllegalArgumentException("FAIL. The old password is incorrect.");
+        // check if the old password is correct
+        if (!customer.getPassword().equals(oldPassword)) {
+            return "FAIL. The old password is incorrect.";
         }
 
+        // check if the two new password inputs match.
         if (!newPassword.equals(confirmNewPassword)) {
-            throw new IllegalArgumentException("FAIL. Password confirmation does not match.");
+            return "FAIL. Password confirmation does not match.";
         }
 
-        customer.setPassword(newPassword);
-        return "SUCCESS new password is: " + customer.getPassword();
+        else {
+            customer.setPassword(newPassword);
+            return "SUCCESS new password is: " + customer.getPassword();
+        }
     }
 
 }
