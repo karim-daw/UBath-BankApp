41c3553139c3a5ec3d9643c12fb2796aca46deeb
karimdaw1991 <karimdaw91@gmail.com>
2023-04-08 11:51:19 +0200

    adding transaciton hashmap

--- newbank/src/main/java/se2/groupb/server/repository/TransactionRepositoryImpl.java
+++ newbank/src/main/java/se2/groupb/server/repository/TransactionRepositoryImpl.java
@@ -1,11 +1,33 @@
 package se2.groupb.server.repository;
 
+import java.util.HashMap;
 import java.util.UUID;
 
 import se2.groupb.server.transaction.Transaction;
 import se2.groupb.server.transaction.TransactionDTO;
 
 public class TransactionRepositoryImpl implements EntityRepository<Transaction, TransactionDTO> {
+
+    private final HashMap<String, Transaction> theTransactions;
+
+    public TransactionRepositoryImpl(HashMap<String, Transaction> transactions) {
+        this.theTransactions = transactions;
+    }
+
+    public HashMap<String, Transaction> getTheTransactions() {
+        return theTransactions;
+    }
+
+    /**
+     * Searches the Trnasaction Data Store by TransactionID
+     * 
+     * @param transactionID
+     * @return Transaction from database
+     */
+    @Override
+    public Transaction findByID(UUID transactionID) {
+        return theTransactions.get(transactionID.toString());
+    }
 
     @Override
     public Transaction findByDTO(TransactionDTO transactionDTO) {
@@ -14,15 +36,22 @@ public class TransactionRepositoryImpl implements EntityRepository<Transaction, 
     }
 
     @Override
-    public Transaction findByID(UUID transactionID) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    public boolean save(Transaction newTransaction) {
+        try {
+            if (newTransaction == null || newTransaction.getTransactionID() == null) {
+                System.out.println("Transaction and TransactionID cannot be null.");
+                return false;
+            }
 
-    @Override
-    public boolean save(Transaction transaction) {
-        // TODO Auto-generated method stub
-        return false;
+            Transaction previousTransaction = theTransactions.put(newTransaction.getTransactionID().toString(),
+                    newTransaction);
+
+            return previousTransaction == null; // return true if put was successful
+
+        } catch (IllegalArgumentException | NullPointerException e) {
+            e.printStackTrace();
+            return false; // error occurred
+        }
     }
 
     @Override
