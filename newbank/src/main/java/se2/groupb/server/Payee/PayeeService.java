6834fd84bf67422db80cad4fc26b713ecaa7c9c1
karimdaw1991 <karimdaw91@gmail.com>
2023-04-10 21:47:11 +0200

    add getters to payee service

--- newbank/src/main/java/se2/groupb/server/Payee/PayeeService.java
+++ newbank/src/main/java/se2/groupb/server/Payee/PayeeService.java
@@ -4,6 +4,27 @@ import java.util.UUID;
 
 public interface PayeeService {
 
+    /**
+     * @param payeeID
+     * @return
+     */
     public Payee getPayeeByID(UUID payeeID);
 
+    /**
+     * @param payeeDTO
+     * @return
+     */
+    public Payee getPayeeByDTO(PayeeDTO payeeDTO);
+
+    /**
+     * @param payeeName
+     * @return
+     */
+    public Payee getPayeeByName(String payeeName);
+
+    /**
+     * @param payeeDTO
+     * @return
+     */
+    public boolean addNewPayee(PayeeDTO payeeDTO);
 }
--- newbank/src/main/java/se2/groupb/server/Payee/PayeeServiceImpl.java
+++ newbank/src/main/java/se2/groupb/server/Payee/PayeeServiceImpl.java
@@ -1,5 +1,31 @@
 package se2.groupb.server.Payee;
 
-public class PayeeServiceImpl {
+import java.util.UUID;
+
+public class PayeeServiceImpl implements PayeeService {
+
+    @Override
+    public boolean addNewPayee(PayeeDTO payeeDTO) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Payee getPayeeByDTO(PayeeDTO payeeDTO) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Payee getPayeeByID(UUID payeeID) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Payee getPayeeByName(String payeeName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
