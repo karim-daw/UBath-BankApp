ea2d84c2c1bd2698abe19893b3bb2db1acb1242b
karimdaw1991 <karimdaw91@gmail.com>
2023-04-10 21:48:06 +0200

    implemented basic payee repo

--- newbank/src/main/java/se2/groupb/server/repository/PayeeRepositoryImpl.java
+++ newbank/src/main/java/se2/groupb/server/repository/PayeeRepositoryImpl.java
@@ -1,5 +1,6 @@
 package se2.groupb.server.repository;
 
+import java.util.HashMap;
 import java.util.UUID;
 
 import se2.groupb.server.Payee.Payee;
@@ -7,34 +8,72 @@ import se2.groupb.server.Payee.PayeeDTO;
 
 public class PayeeRepositoryImpl implements EntityRepository<Payee, PayeeDTO> {
 
+    // fields
+    private final HashMap<String, Payee> thePayees;
+
+    // constrtuctor
+    public PayeeRepositoryImpl(HashMap<String, Payee> thePayees) {
+        this.thePayees = thePayees;
+    }
+
+    // getter
+    public HashMap<String, Payee> getThePayees() {
+        return thePayees;
+    }
+
     @Override
     public Payee findByDTO(PayeeDTO payeeDTO) {
-        // TODO Auto-generated method stub
+        // return thePayees.get(payeeID.toString());
         return null;
     }
 
     @Override
     public Payee findByID(UUID payeeID) {
-        // TODO Auto-generated method stub
-        return null;
+        return thePayees.get(payeeID.toString());
     }
 
     @Override
     public Payee findByName(String payeeName) {
-        // TODO Auto-generated method stub
-        return null;
+        Payee payee = null;
+
+        for (HashMap.Entry<String, Payee> p : thePayees.entrySet()) {
+            String payeeUsername = p.getValue().getPayeeName();
+            if ((payeeName.equals(payeeUsername))) {
+                payee = p.getValue();
+                break;
+            }
+        }
+        return payee;
     }
 
     @Override
-    public boolean save(Payee payee) {
-        // TODO Auto-generated method stub
-        return false;
+    public boolean save(Payee newPayee) {
+        try {
+            if (findByID(newPayee.getPayeeID()) == null) {
+                thePayees.put(newPayee.getPayeeID().toString(), newPayee);
+                return true;
+            } else {
+                return false; // payee already exists
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false; // error occurred
+        }
     }
 
     @Override
-    public boolean update(Payee payee) {
-        // TODO Auto-generated method stub
-        return false;
+    public boolean update(Payee newPayee) {
+        try {
+            if (findByID(newPayee.getPayeeID()) != null) {
+                thePayees.put(newPayee.getPayeeID().toString(), newPayee);
+                return true;
+            } else {
+                return false; // payee already exists
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false; // error occurred
+        }
     }
 
 }
